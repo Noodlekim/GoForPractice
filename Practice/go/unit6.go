dc16eae464f1ab79cb80a1a4ea3b663f17d11727
gibong.kim <gibong.kim@wealthnavi.com>
2017-05-17 18:00:22 +0900

    Add unit7.go file.

--- Practice/go/unit6.go
+++ Practice/go/unit6.go
@@ -1,16 +1,14 @@
 package main
 
-import "fmt"
-
-func main() {
-	i := 10
-	if i >= 5 {
-		fmt.Println("5以上")
-	} else {
-		fmt.Println("5未満")
-	}
-
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-	}
-}
+// func main() {
+// 	i := 10
+// 	if i >= 5 {
+// 		fmt.Println("5以上")
+// 	} else {
+// 		fmt.Println("5未満")
+// 	}
+//
+// 	for i := 0; i < 5; i++ {
+// 		fmt.Println(i)
+// 	}
+// }
---
+++ Practice/go/unit7.go
@@ -0,0 +1,28 @@
+package main
+
+// 변수 선언
+// var i int
+// var s string
+//
+// var age int = 10
+// var name string = "Maria"
+
+// var age = 10
+// var name = "Maria"
+// var address = "Tokyo"
+
+// := 를 이용하면 임시 변수를 만들어 쓸 수 있음.
+// if, for문같은 제어문안에서의 임시변수에서 사용됨.
+// age := 10
+// name := "Kim"
+
+// 복수 변수 선언
+// func main() {
+//
+// 	var x, y int = 10, 20
+// 	var firstName, lastName = "gibong", "kim"
+//
+// 	println(x, y)
+// 	println(firstName, lastName)
+//
+// }
